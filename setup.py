a5fd591b5ccb38a59bacd2594f796b79da170b33
mossishahi <ge65cew@mytum.de>
2025-10-25 12:48:45 +0200

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='SpatialMetric',
+      version='0.1.0',
+      description='SpatialMetric',
+      author='Mostafa Shahhoseini',
+      author_email='shahhosseini.94@gmail.com',
+      url='https://github.com/M0hammadL/SpatialMetric/',
+      packages=['SpatialMetric'],
+     )
